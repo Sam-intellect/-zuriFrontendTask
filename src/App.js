a4fc0c9fa1c26d07083e1ba5c2ecf7d35dd83a8d
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-10-31 12:45:21 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
 import LinksButton from "./linkButton";
 import ShareIcon from "./shareButton.svg";
+import SlackIcon from "./slack.svg";
+import GitHub from "./GitHub.svg";
+import Zuri from "./Zuri.svg";
+import ingressiveLogo from "./I4G.svg";
 import Profile from "./profile__img.png";
 
 const App = () => {
@@ -20,13 +24,30 @@ const App = () => {
               Aluko Olakunle Samuel
             </h2>
           </div>
+          <div className="flex flex-col gap-5 mx-5 mt-10 md:mt-20">
+            <LinksButton link={"Twitter Link"} />
+            <LinksButton link={"Zuri Team"} />
+            <LinksButton link={"Zuri Books"} />
+            <LinksButton link={"Python Books"} />
+            <LinksButton link={"Background Check for Coders"} />
+            <LinksButton link={"Design Books"} />
 
-          <LinksButton link={"Twitter Link"} />
-          <LinksButton link={"Zuri Team"} />
-          <LinksButton link={"Twitter Link"} />
-          <LinksButton link={"Twitter Link"} />
-          <LinksButton link={"Twitter Link"} />
+            <div className="flex flex-row justify-center gap-4">
+              <img src={SlackIcon} alt="share" onClick={() => {}} />
+              <img src={GitHub} alt="share" onClick={() => {}} />
+            </div>
+          </div>
         </div>
+
+        <hr className="bg-grey w-full h-1 mt-32" />
+        {/* Footer */}
+        <footer className="mb-32 mx-10">
+          <div className="flex flex-col gap-4 md:flex-row justify-between mt-4 ">
+            <img src={Zuri} alt="zuri" className=" w-1/2 md:w-1/5 " />
+            <h1 className="md:mt-10">HNG Internship 9 Frontend Task</h1>
+            <img src={ingressiveLogo} alt="zuri" className="w-1/3 md:w-1/5" />
+          </div>
+        </footer>
       </div>
     </>
   );
--- src/linkButton.jsx
+++ src/linkButton.jsx
@@ -3,11 +3,9 @@ import React from "react";
 const LinksButton = (props) => {
   return (
     <>
-      <div className="flex flex-col gap-5 mx-5 mt-20">
-        <button className="font-sand text-center bg-grey p-5 rounded-lg">
-          {props.link}
-        </button>
-      </div>
+      <button className="font-sand text-center bg-grey p-5 md:p-10 font-bold rounded-lg">
+        {props.link}
+      </button>
     </>
   );
 };
