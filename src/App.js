cf25dc5f504853935f236a3925fcd4d450db8463
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-10-31 08:27:25 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <>
+      <div className="container mx-auto bg-gray-200 rounded-xl">
+        <div className="items-center text-center">
+          <p className="text-3xl text-gray-700 font-bold mb-5">Welcome</p>
+          <p className="text-gray-500 text-lg">
+            React and tailwindcss in action
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
