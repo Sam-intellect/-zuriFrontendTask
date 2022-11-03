92f1c50cf00cc7c102c9f51db28f96906547e60e
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-11-03 10:49:14 +0100

    adding React-route-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import "./App.css";
-import LinksButton from "./linkButton";
-import ShareIcon from "./shareButton.svg";
-import MobileIcon from "./mobileDot.svg";
-import SlackIcon from "./slack.svg";
-import GitHub from "./GitHub.svg";
-import Zuri from "./Zuri.svg";
-import ingressiveLogo from "./I4G.svg";
-import Profile from "./profile__img.png";
+import './App.css';
+import LinksButton from './linkButton';
+import ShareIcon from './shareButton.svg';
+import MobileIcon from './mobileDot.svg';
+import SlackIcon from './slack.svg';
+import GitHub from './GitHub.svg';
+import Zuri from './Zuri.svg';
+import ingressiveLogo from './I4G.svg';
+import Profile from './profile__img.png';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -43,34 +44,34 @@ const App = () => {
           </div>
           <div className="flex flex-col gap-5 mx-5 mt-10 md:mt-20">
             <LinksButton
-              name={"Twitter"}
-              links={"https://twitter.com/AlukoKunleSam"}
+              name={'Twitter'}
+              links={'https://twitter.com/AlukoKunleSam'}
             />
 
             <LinksButton
-              name={"Zuri Team"}
+              name={'Zuri Team'}
               links={`https://training.zuri.team/`}
-              id={"btn__zuri"}
+              id={'btn__zuri'}
             />
             <LinksButton
-              name={"Zuri Books"}
+              name={'Zuri Books'}
               links={` http://books.zuri.team `}
-              id={"books"}
+              id={'books'}
             />
             <LinksButton
-              name={"Python Books"}
+              name={'Python Books'}
               links={`https://books.zuri.team//python-for-beginners?ref_id=Sam_dev`}
-              id={"book__python"}
+              id={'book__python'}
             />
             <LinksButton
-              name={"Background Check for Coders"}
+              name={'Background Check for Coders'}
               links={`https://background.zuri.team`}
-              id={"pitch"}
+              id={'pitch'}
             />
             <LinksButton
-              name={"Design Books"}
+              name={'Design Books'}
               links={`https://books.zuri.team/design-rules`}
-              id={"book__design"}
+              id={'book__design'}
             />
 
             <div className="flex flex-row justify-center gap-4">
@@ -86,14 +87,15 @@ const App = () => {
 
         <hr className="bg-grey w-full h-1 mt-32" />
         {/* Footer */}
-        <footer className="mb-32 mx-10">
-          <div className="flex flex-col gap-4 md:flex-row justify-between mt-4 ">
-            <img src={Zuri} alt="zuri" className=" w-1/2 md:w-1/5 " />
-            <h1 className="md:mt-10">HNG Internship 9 Frontend Task</h1>
-            <img src={ingressiveLogo} alt="zuri" className="w-1/3 md:w-1/5" />
-          </div>
-        </footer>
       </div>
+
+      <footer className="mb-32 mx-10">
+        <div className="flex flex-col gap-4 md:flex-row justify-between mt-4 ">
+          <img src={Zuri} alt="zuri" className=" w-1/2 md:w-1/5 " />
+          <h1 className="md:mt-10">HNG Internship 9 Frontend Task</h1>
+          <img src={ingressiveLogo} alt="zuri" className="w-1/3 md:w-1/5" />
+        </div>
+      </footer>
     </>
   );
 };
