6344b277d5c685ce40078e9fd03ea0e94db0c678
Sam-intellect <alukoolakunlesamuel@gmail.com>
2022-11-03 11:46:45 +0100

    Refactor code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import LinksButton from './linkButton';
-import ShareIcon from './shareButton.svg';
-import MobileIcon from './mobileDot.svg';
-import SlackIcon from './slack.svg';
-import GitHub from './GitHub.svg';
-import Zuri from './Zuri.svg';
-import ingressiveLogo from './I4G.svg';
-import Profile from './profile__img.png';
+import LinksButton from './components/linkButton';
+import Footer from './components/Footer';
+import ShareIcon from './assets/shareButton.svg';
+import MobileIcon from './assets/mobileDot.svg';
+import SlackIcon from './assets/slack.svg';
+import GitHub from './assets/GitHub.svg';
+
+import Profile from './assets/profile__img.png';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -73,6 +73,11 @@ const App = () => {
               links={`https://books.zuri.team/design-rules`}
               id={'book__design'}
             />
+            <LinksButton
+              name={'Contact Me'}
+              links={`https://books.zuri.team/design-rules`}
+              id={'contact'}
+            />
 
             <div className="flex flex-row justify-center gap-4">
               <a href="slack.com">
@@ -88,14 +93,7 @@ const App = () => {
         <hr className="bg-grey w-full h-1 mt-32" />
         {/* Footer */}
       </div>
-
-      <footer className="mb-32 mx-10">
-        <div className="flex flex-col gap-4 md:flex-row justify-between mt-4 ">
-          <img src={Zuri} alt="zuri" className=" w-1/2 md:w-1/5 " />
-          <h1 className="md:mt-10">HNG Internship 9 Frontend Task</h1>
-          <img src={ingressiveLogo} alt="zuri" className="w-1/3 md:w-1/5" />
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Zuri from '../assets/Zuri.svg';
+import ingressiveLogo from '../assets/I4G.svg';
+
+const Footer = () => {
+  return (
+    <>
+      <footer className=" container mx-auto md:mt-32 mb-32">
+        <div className="flex flex-col gap-4 md:flex-row justify-between mt-4 ">
+          <img src={Zuri} alt="zuri" className=" w-1/2 md:w-1/5 " />
+          <h1 className="md:mt-10">HNG Internship 9 Frontend Task</h1>
+          <img src={ingressiveLogo} alt="zuri" className="w-1/3 md:w-1/5" />
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
